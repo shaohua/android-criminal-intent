f5538799d1b782283e1ec0a09706f13068421ce0
Shaohua Zhou <shaohua@uber.com>
2015-03-15 11:04:19 -0700

    Finish chapter 11

--- app/src/main/java/com/howtox/mobile/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/howtox/mobile/criminalintent/CrimeListFragment.java
@@ -37,7 +37,7 @@ public class CrimeListFragment extends ListFragment {
         Crime c = ((CrimeAdapter)getListAdapter()).getItem(position);
 
         // start CrimeActivity
-        Intent i = new Intent(getActivity(), CrimeActivity.class);
+        Intent i = new Intent(getActivity(), CrimePagerActivity.class);
         i.putExtra(CrimeFragment.EXTRA_CRIME_ID, c.getId());
         startActivity(i);
     }
